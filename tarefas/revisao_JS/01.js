617298ef2b6d606ef6e9ad23fdc1a58e7eceb9ec
Ivy Richa <ivyricha@gmail.com>
2022-03-31 18:59:12 -0300

    exercícios Módulo 5

---
+++ tarefas/revisao_JS/01.js
@@ -0,0 +1,12 @@
+const array = [1, 5, 23, 76, 87, 34, 687, 86, 98, 103, 489, 423, 89];
+
+function soma(...array){
+    let soma = 0
+    array.map((array) =>{
+        soma += array
+    })
+    return soma
+}
+
+console.log(soma(...array))
+
